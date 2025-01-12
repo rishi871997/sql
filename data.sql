6543aa5611690cd36b7096e8bff2b9fe456ac24a
Rishabh Srivastava <rs871997@gmail.com>
2025-01-12 12:36:29 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset rishabh:004 splitStatements:false stripComments:false
 
-CREATE TABLE new (
+CREATE TABLE first (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -9,5 +9,5 @@ CREATE TABLE new (
     hire_date DATE NOT NULL
 );
 
-INSERT INTO new (first_name, last_name, email, hire_date) VALUES ('Johny', 'Doe', 'john.doe@example.com', '2023-01-15');
-INSERT INTO new (first_name, last_name, email, hire_date) VALUES ('Jane', 'Smith', 'jane.smith@example.com', '2023-03-20');
+INSERT INTO first (first_name, last_name, email, hire_date) VALUES ('Johny', 'Doe', 'john.doe@example.com', '2023-01-15');
+INSERT INTO first (first_name, last_name, email, hire_date) VALUES ('Jane', 'Smith', 'jane.smith@example.com', '2023-03-20');
