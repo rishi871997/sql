7bc5961844c2cb9d58721551884749becc7d8b2a
rishi871997 <rs871997@gmail.com>
2025-01-12 12:16:53 +0530

    first

---
+++ data.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+--changeset rishabh:004 splitStatements:false stripComments:false
+
+CREATE TABLE new (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100),
+    hire_date DATE NOT NULL
+);
+
+INSERT INTO new (first_name, last_name, email, hire_date) VALUES ('Johny', 'Doe', 'john.doe@example.com', '2023-01-15');
+INSERT INTO new (first_name, last_name, email, hire_date) VALUES ('Jane', 'Smith', 'jane.smith@example.com', '2023-03-20');
